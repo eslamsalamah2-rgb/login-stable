ecaabd1dfffe9fff2ed64be28b8152f316f28697
eslamsalamah2-rgb <eslamsalamah2@gmail.com>
2026-09-11 13:18:29 -0700

    Apply Settings language details patch

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ import language_toggle_patch
 import input_speed_patch
 import login_input_settings_patch
 import login_username_arrow_patch
+import settings_language_details_patch
 import login_stability_patch
 import invalid_account_id_wait_patch
 import account_retry_limit_patch
