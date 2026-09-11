8425392bebd8c00a14674fc37e861701634940aa
eslamsalamah2-rgb <eslamsalamah2@gmail.com>
2026-09-11 12:31:20 -0700

    Show startup scan panel without auto-running commands

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import compact_item_selector_ui_patch
 import ui_controls_settings_patch
 import settings_onoff_close_patch
 import top_header_controls_patch
+import startup_scan_visible_patch
 import input_speed_patch
 import login_stability_patch
 import invalid_account_id_wait_patch
