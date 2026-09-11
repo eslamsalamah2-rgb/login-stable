040fb82a252394999820de2cb8a3feeccbef663e
eslamsalamah2-rgb <eslamsalamah2@gmail.com>
2026-09-11 12:16:46 -0700

    Enable configurable key settings and manual start UI controls

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import sash_next_page_patch
 import post_sash_final_actions_patch
 import item_selection_patch
 import compact_item_selector_ui_patch
+import ui_controls_settings_patch
 import input_speed_patch
 import login_stability_patch
 import invalid_account_id_wait_patch
