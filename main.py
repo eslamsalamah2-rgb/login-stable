923dadeb1e488d436c822035608b8e3531d53482
eslamsalamah2-rgb <eslamsalamah2@gmail.com>
2026-09-11 12:28:14 -0700

    Show Start and Stop beside title on startup

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import item_selection_patch
 import compact_item_selector_ui_patch
 import ui_controls_settings_patch
 import settings_onoff_close_patch
+import top_header_controls_patch
 import input_speed_patch
 import login_stability_patch
 import invalid_account_id_wait_patch
